fee34f6e51623567a8d8c5d1465e380b930ca24f
Juris <j.a_turbo@inboxlv>
2018-10-21 10:53:24 +0300

    Piemers

---
+++ Test/src/test/Test.java
@@ -0,0 +1,15 @@
+
+package test;
+
+
+public class Test {
+
+    
+    public static void main(String[] args) {
+        String str = "*******";
+        System.out.println(str);
+        System.out.println(str.toCharArray());
+        
+    }
+    
+}
